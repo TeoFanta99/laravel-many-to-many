ef4433aabf845f7651bf4c86276a0e4e59245c7b
Matteo Fantacuzzi <fantacuzzimatteo@gmail.com>
2024-02-28 16:24:31 +0100

    add component

---
+++ front-end/src/components/ApiContent.vue
@@ -0,0 +1,32 @@
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'ApiContent',
+  mounted() {
+    console.log('ciao');
+
+    axios.get('http://localhost:8000/api/v1/test')
+
+      .then(res => {
+
+        const data = res.data;
+        console.log(data);
+      })
+
+
+      .catch(err => {
+        consol.err(err);
+      })
+
+  }
+}
+
+</script>
+
+<template>
+  <h1>API CONTENT</h1>
+</template>
+
+<style scoped></style>
+ 
